6826126da3f0bc6783c7418376775dab686318ff
TAUWRY <101899708+TAUWRY@users.noreply.github.com>
2022-09-22 22:53:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,125 @@
-const unirest = require("unirest");
-const cheerio = require("cheerio");
-const company= 'enhabit'     //company to google
-const keyword= 'enhabit'     //keyword to search within the returned results
-const zurl= `https://www.google.com/search?q=${company}&tbm=nws&source=lnt&tbs=qdr:h&sa`
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app= express()
+var repeatCounter=0
+const {stockName}= require('./../../personalcoding/stock-list.js')
+const {arrExclude}= require('./../../personalcoding/stock-exclude.js')
+const url = 'https://www.globenewswire.com/NewsRoom'
 
-const getNewsData = () => {
-  return unirest
-    .get(zurl)
-    .headers({
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
-    })
-    .then((response) => {
-      let $ = cheerio.load(response.body);
-      let news_results = []; 
-
-  $(".BGxR7d").each((i,el) => {
-    news_results.push({
-     //link: $(el).find("a").attr('href'),
-     title: $(el).find("div.mCBkyc").text(),
-     //date: $(el).find(".ZE0LJd span").text(),
-    })
-  })
-
-  for(let i=0;i<news_results.length;i++) {
-    var titlez= news_results[i].title
-    const myRegex= new RegExp(keyword, "gi")
-    let result = myRegex.test(titlez)
-    if(result){
-      function party() {
-        console.log('\x07')
-        console.log("MATCH FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        for(let j=0;j<news_results.length;j++) {
-          var titlez2= news_results[j].title
-          let result2 = titlez2.match(myRegex)
-          if(result2) {
-            console.log(titlez2)
-          }
+function funcFullScrape() {
+    axios(url)
+    .then(response=> {
+        const html = response.data
+        const $= cheerio.load(html)
+        const arrAllTitles=[]
+        let arrFilteredTitles= []
+        let arrMatch=[]
+        let stringMatch
+        $('.pagging-list-item-text-container', html).each(function(){
+            const scrapeTitle = $(this).find('a').attr('href').replace(/-/g, ' ').replace(/news release/g,'').replaceAll('//','')
+            arrAllTitles.push({
+                scrapeTitle
+            })
+        })
+        if(arrAllTitles[0]==undefined) {
+            return console.log('ERROR: TAG NOT FOUND----ERROR: TAG NOT FOUND----ERROR: TAG NOT FOUND')
         }
-        setTimeout(party, 3000)
-      }
-      return party()
-    }
-  }
-      console.log(news_results)
-      console.log('NOTHING FOUND___________NOTHING FOUND___________NOTHING FOUND')
-      setTimeout(getNewsData, 60000)
-      //console.log(news_results)
-    });
-};
+        for(let i=0;i<arrAllTitles.length;i++) {
+            let stringTitle= arrAllTitles[i].scrapeTitle.toLowerCase()
+            let myRegex= new RegExp(stockName.join('|'), "gi")
+            for(let k=0;k<arrExclude.length;k++){
+                if(stringTitle.includes(arrExclude[k].toLowerCase())) {
+                    stringTitle= '@@@!!@@@'
+                }
+            }
+            stringMatch = stringTitle.match(myRegex)
+            if(stringMatch) {
+                arrFilteredTitles.push(stringTitle)
+                arrMatch.push(stringMatch[0])
+            }
+        }
+        if(arrFilteredTitles[0]) {
+            const funcSuccess= () => console.log(arrFilteredTitles, arrMatch,  "\n", '\x07', "\n", 
+            `Run count: ${repeatCounter+=1}.....GLOBAL-NEWSWIRE-MATCH FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+            funcSuccess()
+            setTimeout(funcSuccess, 6000)
+            setTimeout(funcSuccess, 12000)
+            return setTimeout(funcFullScrape, 10000)
+        } else {
+            console.log(`Scraper bot running..........${repeatCounter+=1}`)
+            setTimeout(funcFullScrape, 5000)
+        }
+    }).catch(err => console.log(err))
+}
+funcFullScrape()
+app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
 
-getNewsData();
+
+
+
+
+
+
+// const PORT = 8000
+//     const axios = require('axios')
+//     const cheerio = require('cheerio')
+//     const express = require('express')
+//     const app= express()
+//     var count1=0
+//     const url = 'https://www.globenewswire.com/NewsRoom'
+//     const {keyword}= require('./../../personalcoding/stock-list.js')
+// function gobo() {
+// axios(url)
+//     .then(response=> {
+//         const html = response.data
+//         const $= cheerio.load(html)
+//         const articles=[]
+//         $('.pagging-list-item-text-container', html).each(function(){
+//             const title = $(this).find('a').attr('href').replace(/-/g, ' ').replace(/news release/g,'').replaceAll('//','')
+//             //const zurl= $(this).find('a').attr('href')
+//             //const title = $(this).text().replace(/n/, '')
+//             articles.push({
+//                 title
+//             })
+//         })
+//         let result
+//         let arr1= []
+//         for(let i=0;i<articles.length;i++) {
+//             var titlez= articles[i].title
+//             var myRegex= new RegExp(keyword.join('|'), "gi")
+//             if(titlez.includes(' depokj ')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('Conference')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('Financial Results')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('First Quarter')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('Second Quarter')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('Third Quarter')) {
+//                 titlez= '@@@@@@'
+//             } else if(titlez.includes('Fourth Quarter')) {
+//                 titlez= '@@@@@@'
+//             } 
+//             result = titlez.match(myRegex)
+//             if(result) {
+//                 arr1.push(titlez)
+//             }
+//         }
+//         if(arr1[0]) {
+//         var show1= () => console.log(arr1,'\x07', "\n", `Run count: ${count1+=1}.....GLOBAL-NEWSWIRE-MATCH FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+//         show1()
+//         setTimeout(show1, 6000)
+//         setTimeout(show1, 12000)
+//         setTimeout(gobo, 40000)
+//         } else {
+//             console.log(`Scraper bot running..........${count1+=1}`)
+//             setTimeout(gobo, 40000)
+//         }
+//     }).catch(err => console.log(err))
+// }
+// gobo()
+// app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
